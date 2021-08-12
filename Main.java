8f08c95d9bac072a474676938a14307e3c57ebc5
IvanBurk <64144110+IvanBurk@users.noreply.github.com>
2021-08-12 16:05:55 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5353;
+        int quantity = 20;
+        int bonus = price / quantity;
+        System.out.println(bonus);
+    }
+}
